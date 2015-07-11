0991a294c1092a092451629fcda5f0b4deae2090
Kelly Munro <kellymdev@gmail.com>
2015-07-11 12:00:21 +1200

    Remove faker gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
 
-require 'faker'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
